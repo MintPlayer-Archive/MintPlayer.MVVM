ad1f77d6d9977028b4be9b304520c839d2621d65
MusicDemons <pieterjandeclippel@hotmail.com>
2020-07-27 22:06:48 +0200

    Add demo project

---
+++ Demo/MintPlayer.MVVM.Demo/MintPlayer.MVVM.Demo/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MintPlayer.MVVM.Demo.Services;
+using MintPlayer.MVVM.Demo.Views;
+
+namespace MintPlayer.MVVM.Demo
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Demo/MintPlayer.MVVM.Demo/MintPlayer.MVVM.Demo/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MintPlayer.MVVM.Demo.Models;
+
+namespace MintPlayer.MVVM.Demo.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
