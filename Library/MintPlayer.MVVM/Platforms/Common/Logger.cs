4f13ef68c22ff3c7ca72acb69dcd1eafc61b0445
PieterjanDeClippel <pieterjandeclippel@msn.com>
2020-09-08 11:01:55 +0200

    Added greeters + logger

---
+++ Library/MintPlayer.MVVM/Platforms/Common/Logger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MintPlayer.MVVM.Platforms.Common
+{
+    public static class Logger
+    {
+        public static void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ Library/MintPlayer.MVVM/Platforms/iOS/Greeter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MintPlayer.MVVM.Platforms.Android
+{
+    public static class Greeter
+    {
+        public static void SayHello()
+        {
+            Console.WriteLine("Hello from iOS");
+        }
+    }
+}
