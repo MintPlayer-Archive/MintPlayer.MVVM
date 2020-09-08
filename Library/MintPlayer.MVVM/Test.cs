89e134fbce51d7b4d1793829eda53ca03c03f64b
PieterjanDeClippel <pieterjandeclippel@msn.com>
2020-09-08 10:44:50 +0200

    Re-initialize code

---
+++ Library/MintPlayer.MVVM/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MintPlayer.MVVM
+{
+    public static class Test
+    {
+        public static void WriteLine(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
