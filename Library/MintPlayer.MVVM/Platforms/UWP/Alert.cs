5192b6dc962cacdb51ba87c62defbbca363a16f6
PieterjanDeClippel <pieterjandeclippel@msn.com>
2020-09-08 17:01:15 +0200

    Implement platform-specific calls

---
+++ Library/MintPlayer.MVVM/Platforms/UWP/Alert.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace MintPlayer.MVVM.Platforms.UWP
+{
+    public static class Alert
+    {
+        public static async Task Show(string text)
+        {
+            var dialog = new MessageDialog(text);
+            await dialog.ShowAsync();
+        }
+    }
+}
--- Library/MintPlayer.MVVM/Platforms/UWP/Greeter.cs
+++ Library/MintPlayer.MVVM/Platforms/UWP/Greeter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Windows.UI.Popups;
 
 namespace MintPlayer.MVVM.Platforms.UWP
 {
