26e2a387c90b71c46a2d0d4edc32241ec799a492
DESKTOP-K4FBQRK\piete <pieterjandeclippel@msn.com>
2020-10-09 14:26:08 +0200

    Moved the SetProperty method to BaseModel

--- Demo/MintPlayer.MVVM.Demo/MintPlayer.MVVM.Demo/ViewModels/BaseVM.cs
+++ Demo/MintPlayer.MVVM.Demo/MintPlayer.MVVM.Demo/ViewModels/BaseVM.cs
@@ -20,16 +20,5 @@ namespace MintPlayer.MVVM.Demo.ViewModels
             get => title;
             set => SetProperty(ref title, value);
         }
-
-        protected bool SetProperty<T>(ref T backingStore, T value, [CallerMemberName] string propertyName = "", Action onChanged = null)
-        {
-            if (EqualityComparer<T>.Default.Equals(backingStore, value))
-                return false;
-
-            backingStore = value;
-            onChanged?.Invoke();
-            OnPropertyChanged(propertyName);
-            return true;
-        }
     }
 }
